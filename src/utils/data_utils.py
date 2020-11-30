bb141b70c03e9ab7faa6687a1d5ad0c667b2926f
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2020-11-30 18:39:36 +0100

    Update data_utils.py to reflect diverse typing possible with the Vocab class

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -17,18 +17,18 @@ END_TOKEN_IDX = 2
 class Vocab:
     def __init__(self,
                  names: Iterable,
-                 default: str = UNK_TOKEN,
-                 start: int = START_TOKEN,
-                 end: int = END_TOKEN) -> None:
+                 default: Union[int, str] = UNK_TOKEN,
+                 start: Union[int, str] = START_TOKEN,
+                 end: Union[int, str] = END_TOKEN) -> None:
         self.default = default
         self.names = list(nub(chain([default, start, end], names)))
         self.index = {name: i for i, name in enumerate(self.names)}
 
-    def __getitem__(self, index: int) -> str:
+    def __getitem__(self, index: int) -> Union[int, str]:
         return self.names[index] if 0 < index < len(
             self.names) else self.default
 
-    def __call__(self, name: str) -> int:
+    def __call__(self, name: Union[int, str]) -> int:
         return self.index.get(name, UNK_IDX)
 
     def __contains__(self, item: str) -> bool:
@@ -40,21 +40,22 @@ class Vocab:
     def __or__(self, other: 'Vocab') -> 'Vocab':
         return Vocab(self.names + other.names)
 
-    def numberize(self, doc: List[str]) -> List[int]:
+    def numberize(self, doc: List[Union[int, str]]) -> List[int]:
         return [self(token) for token in doc]
 
-    def denumberize(self, doc: List[int]) -> List[str]:
+    def denumberize(self, doc: List[int]) -> List[Union[int, str]]:
         return [self[idx] for idx in doc]
 
-    @staticmethod
-    def from_docs(docs: List[List[str]],
-                  default: str = UNK_TOKEN,
-                  start: str = START_TOKEN,
-                  end: str = END_TOKEN) -> 'Vocab':
-        return Vocab((i for doc in docs for i in doc),
-                     default=default,
-                     start=start,
-                     end=end)
+    @classmethod
+    def from_docs(cls,
+                  docs: List[List[Union[int, str]]],
+                  default: Union[int, str] = UNK_TOKEN,
+                  start: Union[int, str] = START_TOKEN,
+                  end: Union[int, str] = END_TOKEN) -> 'Vocab':
+        return cls((i for doc in docs for i in doc),
+                   default=default,
+                   start=start,
+                   end=end)
 
 
 def is_printable(word: str) -> bool:
