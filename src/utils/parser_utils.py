2331346de34680f9db38fc1f13225404c94e368e
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2021-01-03 22:17:34 +0100

    Adding new workflow to pretty-sort argparse usage script

--- src/utils/parser_utils.py
+++ src/utils/parser_utils.py
@@ -28,6 +28,120 @@ class Sorting_Help_Formatter(argparse.HelpFormatter):
         actions = sorted(actions, key=attrgetter('option_strings'))
         super(Sorting_Help_Formatter, self).add_arguments(actions)
 
+    def _format_usage(self, usage: str, actions: Iterable[argparse.Action],
+                      groups: Iterable[argparse._ArgumentGroup],
+                      prefix: Union[str, None]) -> str:
+        if prefix is None:
+            prefix = ('usage: ')
+
+        # if usage is specified, use that
+        if usage is not None:
+            usage = usage % dict(prog=self._prog)
+
+        # if no optionals or positionals are available, usage is just prog
+        elif usage is None and not actions:
+            usage = '%(prog)s' % dict(prog=self._prog)
+
+        # if optionals and positionals are available, calculate usage
+        elif usage is None:
+            prog = '%(prog)s' % dict(prog=self._prog)
+
+            # split optionals from positionals
+            optionals = []
+            positionals = []
+
+            # split actions temporarily
+            help_action = [
+                action for action in actions if action.dest == "help"
+            ]
+            required_actions = sorted(
+                [action for action in actions if action.required],
+                key=attrgetter('option_strings'))
+            optional_actions = sorted([
+                action for action in actions
+                if not action.required and action.dest != "help"
+            ],
+                                      key=attrgetter('option_strings'))
+
+            # combine actions back
+            actions = help_action + required_actions + optional_actions
+
+            # proceed with usual
+            for action in actions:
+                if action.option_strings:
+                    optionals.append(action)
+                else:
+                    positionals.append(action)
+
+            # build full usage string
+            format = self._format_actions_usage
+            action_usage = format(optionals + positionals, groups)
+            usage = ' '.join([s for s in [prog, action_usage] if s])
+
+            # wrap the usage parts if it's too long
+            text_width = self._width - self._current_indent
+            if len(prefix) + len(usage) > text_width:
+
+                # break usage into wrappable parts
+                part_regexp = (r'\(.*?\)+(?=\s|$)|'
+                               r'\[.*?\]+(?=\s|$)|'
+                               r'\S+')
+                opt_usage = format(optionals, groups)
+                pos_usage = format(positionals, groups)
+                opt_parts = re.findall(part_regexp, opt_usage)
+                pos_parts = re.findall(part_regexp, pos_usage)
+                assert ' '.join(opt_parts) == opt_usage
+                assert ' '.join(pos_parts) == pos_usage
+
+                # helper for wrapping lines
+                def get_lines(parts, indent, prefix=None):
+                    lines = []
+                    line = []
+                    if prefix is not None:
+                        line_len = len(prefix) - 1
+                    else:
+                        line_len = len(indent) - 1
+                    for part in parts:
+                        if line_len + 1 + len(part) > text_width and line:
+                            lines.append(indent + ' '.join(line))
+                            line = []
+                            line_len = len(indent) - 1
+                        line.append(part)
+                        line_len += len(part) + 1
+                    if line:
+                        lines.append(indent + ' '.join(line))
+                    if prefix is not None:
+                        lines[0] = lines[0][len(indent):]
+                    return lines
+
+                # if prog is short, follow it with optionals or positionals
+                if len(prefix) + len(prog) <= 0.75 * text_width:
+                    indent = ' ' * (len(prefix) + len(prog) + 1)
+                    if opt_parts:
+                        lines = get_lines([prog] + opt_parts, indent, prefix)
+                        lines.extend(get_lines(pos_parts, indent))
+                    elif pos_parts:
+                        lines = get_lines([prog] + pos_parts, indent, prefix)
+                    else:
+                        lines = [prog]
+
+                # if prog is long, put it on its own line
+                else:
+                    indent = ' ' * len(prefix)
+                    parts = opt_parts + pos_parts
+                    lines = get_lines(parts, indent)
+                    if len(lines) > 1:
+                        lines = []
+                        lines.extend(get_lines(opt_parts, indent))
+                        lines.extend(get_lines(pos_parts, indent))
+                    lines = [prog] + lines
+
+                # join lines into usage
+                usage = '\n'.join(lines)
+
+        # prefix with 'usage:'
+        return '%s%s\n\n' % (prefix, usage)
+
 
 class Metavar_Circum_Symbols(argparse.HelpFormatter):
     """
