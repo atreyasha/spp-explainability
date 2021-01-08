1e2627744e800fe03a0367b1e3b5297a1fb53427
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2021-01-08 17:49:02 +0100

    New evaluation shell scripts

---
+++ scripts/evaluate_spp_gpu.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -e
+
+# usage function
+usage() {
+  cat <<EOF
+Usage: evaluate_spp_gpu.sh [-h|--help] model_checkpoint
+
+Evaluate SoPa++ model on an evaluation data set on the GPU
+
+Optional arguments:
+  -h, --help                    Show this help message and exit
+
+Required arguments:
+  model_checkpoint <file_path>  Path to model checkpoint with '.pt'
+                                extension. Note that 'model_config.json'
+                                must be in the same directory level as
+                                the model checkpoint file
+EOF
+}
+
+# check for help
+check_help() {
+  for arg; do
+    if [ "$arg" == "--help" ] || [ "$arg" == "-h" ]; then
+      usage
+      exit 0
+    fi
+  done
+}
+
+# define function
+evaluate_spp_gpu() {
+  local model_checkpoint
+  model_checkpoint="$1"
+
+  python3 -m src.evaluate_spp \
+    --eval-data "./data/facebook_multiclass_nlu/clean/test.uncased.data" \
+    --eval-labels "./data/facebook_multiclass_nlu/clean/test.labels" \
+    --model-checkpoint "$model_checkpoint" --gpu
+}
+
+# execute function
+check_help "$@"
+evaluate_spp_gpu "$@"
