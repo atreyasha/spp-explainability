83fe87995033f74af7f4fccbe4fed9430c547262
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2021-02-09 20:56:36 +0100

    Update argument parser
    Update value of patterns hyperparameter to reflect the value of the best
    performing neural model so far

--- src/arg_parser.py
+++ src/arg_parser.py
@@ -13,7 +13,7 @@ def spp_model_arg_parser() -> argparse.ArgumentParser:
         "--patterns",
         help=("Pattern lengths and counts with the following syntax: " +
               "PatternLength1-PatternCount1_PatternLength2-PatternCount2_..."),
-        default="6-25_5-25_4-25_3-25",
+        default="6-50_5-50_4-50_3-50",
         type=str)
     sopa.add_argument("--semiring",
                       help="Specify which semiring to use",
