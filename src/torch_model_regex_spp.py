44db2d4e10ac573f25fbdd69e99c89cb9d3269cf
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2021-02-10 19:07:28 +0100

    Adding local explainability to SoPa++ regex model
    Creating trace workflow for SoPa++ regex model, allowing it to be easily
    used in model comparison script which is coming up next

--- src/torch_model_regex_spp.py
+++ src/torch_model_regex_spp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from typing import List, Dict
+from typing import List, Dict, Tuple, Union
 from collections import OrderedDict
 from torch.nn import Module
 import torch
@@ -23,22 +23,38 @@ class RegexSoftPatternClassifier(Module):
         }
         self.linear = linear
 
-    def regex_forward(self, doc: str) -> List[int]:
-        scores_doc = []
+    def regex_lookup(self,
+                     doc: str) -> Tuple[List[int], List[Union[int, None]]]:
+        scores_doc: List[int] = []
+        index_doc: List[Union[int, None]] = []
         for key in sorted(self.activating_regex.keys()):
-            for regex in self.activating_regex[key]:
+            for index, regex in enumerate(self.activating_regex[key]):
                 if regex.search(doc):
                     scores_doc.append(1)
+                    index_doc.append(index)
                     break
             else:
                 scores_doc.append(0)
-        return scores_doc
+                index_doc.append(None)
+        return scores_doc, index_doc
 
     def forward(self, batch: List[str]) -> torch.Tensor:
         # start loop over regular expressions
-        scores = torch.FloatTensor(
-            [self.regex_forward(doc)
-             for doc in batch]).to(self.linear.weight.device)  # type: ignore
+        scores = torch.FloatTensor([
+            self.regex_lookup(doc)[0] for doc in batch
+        ]).to(self.linear.weight.device)  # type: ignore
 
         # convert scores to tensor
         return self.linear(scores)
+
+    def forward_with_trace(
+            self, batch: List[str]
+    ) -> Tuple[torch.Tensor, List[List[Union[int, None]]]]:
+        # start loop over regular expressions
+        all_data = [self.regex_lookup(doc) for doc in batch]
+        scores = torch.FloatTensor([data[0] for data in all_data]).to(
+            self.linear.weight.device)  # type: ignore
+        indices = [data[1] for data in all_data]
+
+        # convert scores to tensor
+        return self.linear(scores), indices
