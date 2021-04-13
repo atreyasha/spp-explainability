b11e313baf087b2f9add921c972590e4080c6f66
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2021-04-13 14:37:47 +0200

    Update visualize_grid_evaluate shell script
    Remove "neural SoPa++" mention

--- scripts/visualize_grid_evaluate.sh
+++ scripts/visualize_grid_evaluate.sh
@@ -6,8 +6,8 @@ usage() {
   cat <<EOF
 Usage: visualize_grid_evaluate.sh [-h|--help] model_log_directory
 
-Visualize grid evaluations for neural SoPa++ and regex
-model pairs, given that grid allows for the following varying arguments:
+Visualize grid evaluations for SoPa++ and regex model pairs, given
+the grid-search allows for the following varying arguments:
 patterns, tau_threshold, seed
 
 Optional arguments:
@@ -15,8 +15,8 @@ Optional arguments:
 
 Required arguments:
   model_log_directory <glob_path>  Model log directory/directories
-                                   containing neural SoPa++ and regex
-                                   models, as well as all evaluation json's
+                                   containing SoPa++ and regex models,
+                                   as well as all evaluation json's
 EOF
 }
 
